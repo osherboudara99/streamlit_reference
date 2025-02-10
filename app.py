11fa418783acca3f0074b8155278e4139f2c4c19
osherboudara99 <osherboudara99@gmail.com>
2025-02-10 14:17:07 -0500

    completed streamlit basic notes, cache and session states

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import time
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Test Element 
 st.text("This is a text")
@@ -98,3 +100,194 @@ if file_upload:
     st.write(pd.read_csv(file_upload))
 
 st.download_button("Download Text File", "hello user!")
+
+
+# Status Elements
+if st.button("Compute"):
+    with st.spinner("Thinking"):
+        time.sleep(3)
+        st.write("Hello")
+    
+
+    progress_bar = st.progress(0)
+    for i in range(1, 100):
+        time.sleep(0.03)
+        progress_bar.progress(i, text='Progressing')
+    progress_bar.empty()
+    st.write("Done")
+
+    # Popup element
+    st.toast("This is a toast")
+
+
+# Chat Elements (LLM UI)
+def stream_data(data,delay:float=0.5):
+    for word in data.split():
+        yield word + " "
+        time.sleep(delay)
+
+prompt = st.chat_input("Ask something")
+if prompt: 
+    with st.chat_message("user"):
+        st.write(f"You typed {prompt}")
+
+
+if st.button("Stream"):
+    with st.spinner("Thinking..."):
+        time.sleep(2)
+        response = 'some random text to display'
+        st.write_stream(stream_data(response))
+
+# Streaming response
+# Generator
+# Some text or data
+
+st.divider()
+## Layouts
+# Tabs
+home_tab, about_tab = st.tabs(["Home", "About"])
+
+with home_tab:
+    st.subheader("This is the Home Tab")
+
+with about_tab:
+    st.subheader("This is the About Tab")
+    st.dataframe(df)
+
+
+# Columns
+col1, col2, col3 = st.columns(3)
+# Context Manager approach for columns
+with col1:
+    st.title("Columns 1")
+
+col2.dataframe(df)
+col2.write("this is column 2")
+col3.image("serj.png", use_container_width=True)
+
+# Container
+container = st.container(border=True)
+container.write("My first container")
+
+row1 = st.columns(3)
+row2 = st.columns(3)
+
+for col in row1 + row2:
+    tile = col.container(height=120)
+    tile.title(":balloon:")
+
+
+# Expander and Popover 
+with st.expander("Expand"):
+    st.dataframe(df)
+
+with st.popover("Popover"):
+    st.image("serj.png")
+
+### Plots
+st.area_chart(data=df, x='number', y='letter')
+
+st.line_chart(data=df, x='number', y='letter')
+
+st.bar_chart(data=df, x='number', y='letter')
+
+st.scatter_chart(data=df, x='number', y='letter')
+
+# Matplotlib
+# st.pyplot(df["number"].value_counts().plot(kind="bar"))
+
+#Plotly
+# st.plotly_chart() 
+
+#st.altair_chart()
+#st.map()
+#st.bokeh_chart()
+
+st.help(st.cache_data)
+st.write(dir(st))
+
+with st.form("Form"):
+    first_name = st.text_input("Enter First Name")
+    password = st.text_input("Enter password", type='password')
+    message = st.text_area('Some message')
+    button = st.form_submit_button("Submit")
+
+
+import streamlit.components.v1 as stc 
+
+stc.html("<p> hello </p>")
+# stc.iframe()
+
+st.link_button("visit", url="https://Google.com")
+
+
+# Helps process large data
+# Stores copy of resource
+# If you are downloaded data from elsewhere or performing lots
+# Calculations, to avoid rerun, use cache
+# Functions rerun if function arguments change or if code changes (while developing) 
+
+
+# st.cache_data, used for anything else (calculations, loading data)
+
+@st.cache_data
+def inference(data, random = 0):
+    st.write(f"{data} time:", time.time())
+
+inference('cached')
+
+inference("non-cached", np.random.randn(1, 2))
+
+
+# st.cache_resource, used for loading a model (DL, ML, LLM)
+
+# #Example loading pretrained model
+# @st.cache_resource
+# def load_model(data):
+#     # Model contained in another file
+#     from full_model import Net
+#     st.write(time.time())
+#     model = Net()
+#     checkpoints = torch.load("model_path")
+#     model.load_state_dict(checkpoints["model_state"])
+#     return model
+
+# model = load_model(np.random.randn(1,2)) # since random, always changing, function rerun each time
+
+
+
+# st.session_state
+# Dictionary that Retains values across reruns
+
+st.write(st.session_state)
+if "key" not in st.session_state:
+    st.session_state["key"] = 1
+st.write(st.session_state)
+
+if "key" in st.session_state:
+    st.session_state["key"] = 2
+st.write(st.session_state)
+
+if st.session_state["key"] == 2:
+    del st.session_state["key"]
+st.write(st.session_state)
+
+
+st.text_input('Name', key='name_from_text_input')
+st.session_state
+
+# Slider/checbox value will cause rerun since no submit button
+st.slider("My slider", 0, 10, 1, key='my_slider')
+st.checkbox("Yes or No", key="my checkbox")
+
+def form_callback():
+    st.write(st.session_state['my_slider_form'])
+    st.write(st.session_state['my_checkbox_form'])
+
+# Rerun halted until submit
+with st.form(key = 'my_form'):
+    st.slider("My slider", 0, 10, 1, key='my_slider_form')
+    st.checkbox("Yes or No", key="my_checkbox_form")
+    st.form_submit_button("Submit", on_click = form_callback)
+
+
